f26099a4bc4c91cfc3e4aa6f9f92d23c2582b908
mahmoodhamid <>
2021-08-04 00:09:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# # num_char=len(input("what is your name"))
+# # new_num_char=(str(num_char))
+# # print("my name is "+new_num_char+"  dgsg")
+# # # print(type(num_char))
+
+# a=str(123)
+# print(type(a))
+
+pa=(input("Type a two digit number : "))
+p=pa[0]
+a=pa[1]
+
+print(int(p)+int(a))
